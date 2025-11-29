6f21b74c0203425abfa25e23a0ed2554ade423a9
mrmoe28 <mrmoe28@gmail.com>
2025-11-29 16:20:03 -0500

    Initial Commit

---
+++ Calenda NotesTests/Calenda_NotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Calenda_NotesTests.swift
+//  Calenda NotesTests
+//
+//  Created by Edward Harrison on 11/29/25.
+//
+
+import Testing
+@testable import Calenda_Notes
+
+struct Calenda_NotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Calenda NotesUITests/Calenda_NotesUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Calenda_NotesUITestsLaunchTests.swift
+//  Calenda NotesUITests
+//
+//  Created by Edward Harrison on 11/29/25.
+//
+
+import XCTest
+
+final class Calenda_NotesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
